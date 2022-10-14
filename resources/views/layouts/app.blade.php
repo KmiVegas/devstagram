50dc1c99fa8f9aa162e2c9a462c9cdf3f84fa748
KmiVegas <vegas.camila@gmail.com>
2022-10-14 02:30:14 -0300

    add Tailwind, postcss and autoprefixer

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>DevStagram - @yield('titulo')</title>
+        @vite('resources/css/app.css')
+    </head>
+    <body>
+        <nav>
+            <a href="/">Principal</a>
+            <a href="/nosotros">Nosotros</a>
+            <a href="/tienda">Tienda</a>
+        </nav>
+        <h1 class="text-4xl">@yield('titulo')</h1>
+        <hr>
+        @yield('contenido')
+    </body>
+</html>
